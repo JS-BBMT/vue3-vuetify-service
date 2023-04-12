f81fa861862e16a9934220c27ede2efb60680079
RC6799 <richcool.6799@gmail.com>
2023-04-12 12:59:41 +0900

    moving commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app>
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+      <v-toolbar-title @click="$router.push('/')">Home</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row>
+    <v-col class="d-flex flex-column text-center">
+      <div class="text-center">Starter Vue-3 Service</div>
+      <v-btn @click="handlefetch" color="primary" class="mt-3"
+        >Get Random User</v-btn
+      >
+      <div>{{ getFetch || "-" }}</div>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+import { computed, onMounted } from "vue-demi";
+import { useFetchStore } from "../stores/fetch";
+
+const fetchStore = useFetchStore();
+
+const getFetch = computed(() => fetchStore.getFetch);
+
+const handlefetch = () => {
+  fetchStore.getFetchServices();
+};
+</script>
